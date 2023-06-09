b241bd677b2ef19bf28c96f7d6f03d58717f4c92
junwoo0730 <136076692+junwoo0730@users.noreply.github.com>
2023-06-09 19:17:36 +0900

    Add files via upload

---
+++ 1주차 숙제1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+void main ()
+{
+	printf ("ABCDEFG\n1234567");
+	printf ("\n\n");
+	printf ("abcdefg\r1234");
+	return 0;
+}
+*/
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+void main ()
+{
+	printf ("1.\t2.\t3.\t\n");
+	printf ("CPU\tMemory\tHDD");
+	return 0;
+}
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+void main ()
+{
+	printf ("ABCabc123@#$");
+	printf ("\n\n");
+	printf ("ABC\abc\b123\b@#$");
+	return 0;
+}
---
+++ 1주차 숙제2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+	printf ("##\n") ;
+	printf ("  ¢½¢½      ¢½¢½\n");
+	printf ("¢½¢½¢½¢½  ¢½¢½¢½¢½\n");
+	printf (" ¢½¢½¢½¢½¢½¢½¢½¢½\n");
+	printf ("  ¢½¢½¢½¢½¢½¢½¢½\n");
+	printf ("   ¢½¢½¢½¢½¢½¢½\n");
+	printf ("    ¢½¢½¢½¢½\n");
+	printf ("     ¢½¢½¢½\n");
+	printf ("      ¢½\n");
+	return 0;
+}
