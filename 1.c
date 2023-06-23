8fdc3e24dfd4de296bb1bfa513d08554dbf53380
junwoo0730 <136076692+junwoo0730@users.noreply.github.com>
2023-06-23 20:50:30 +0900

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,121 @@
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	int a;
+	int b;
+	double c;
+	double d;
+	scanf ("%d %d", &a, &b);
+	printf ("%d\n", a+b);
+	printf ("%d\n", a-b);
+	printf ("%d\n", a*b);
+	printf ("몫 : %d, 나머지 : %d\n", a/b, a%b);
+	scanf ("%lf %lf", &c, &d);
+	printf ("%lf\n", c+d);
+	printf ("%lf\n", c-d);
+	printf ("%lf\n", c*d);
+	printf ("%lf", c/d);
+	
+	return 0;
+}
+*/
+/*
+#include <stdio.h>
+#include <stdlib.h>
+int main ()
+{
+	printf ("%d\n", 10/3);
+	printf ("%d\n", 10/3.0);
+	printf ("%lf\n", 10/3.0);
+	return 0;
+	
+}
+*/
+/*
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int a;
+	int b;
+	a = 12;
+	b = 23;
+	printf ("%d\n", 12+23);
+	printf ("%d\n", a+23);
+	printf ("%d\n", a+b);
+	return 0;	
+}
+*/
+/*
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int a = 70, b = 7;
+	double f1 = 77.0, f2 = 7.0;
+	
+	printf ("%d\n", a/b);
+	printf ("%lf\n", f1/b);
+	printf ("%lf\n", a/f2);
+	printf ("%lf\n", f1/f2);
+	return 0;
+	
+}
+*/
+/*
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int a, b, c;
+	
+	printf ("정수 입력 : ");
+	scanf ("%d", &a);
+	printf ("정수 입력 : ");
+	scanf ("%d", &b);
+	printf ("%d\n", 10%3);
+	printf ("%d\n", a%10);
+	c = a%b;
+	printf ("%d\n", c);
+	return 0;
+}
+*/
+/*
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int normal;
+	int vip;
+	int seat;
+	int water;
+	int air;
+	int guard;
+	int guide;
+	int gift;
+	scanf ("%d %d", &normal, &vip);
+	
+	seat = vip + normal/5;
+	water = (normal + vip)/35;
+	air = (normal + vip)/35;
+	guard = (normal + vip)/53 + (normal + vip)/100;
+	guide = normal/100 + vip/20;
+	gift = normal*2 + vip*4;
+	
+	
+	printf ("", )
+}
+*/
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	printf ("%d\n", 34/5);
+	printf ("%lf\n", 34/5.0);
+	printf ("%lf\n", 34.0/5);
+	printf ("%lf\n", 34.0/5.0);
+	return 0;
+}
