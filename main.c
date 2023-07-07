ce4c3f043f36d510cca010a147e2cd25e566cad9
junwoo0730 <136076692+junwoo0730@users.noreply.github.com>
2023-07-07 20:49:02 +0900

    Add files via upload

--- main.c
+++ main.c
@@ -3,14 +3,130 @@
 #include <stdlib.h>
 
 
+
 int main() {
+	int a = 20;
+	
+	a = a+10; 
+	printf ("a = %d\n", a);
+	a = a*10;
+	printf ("a = %d\n", a);
+	a = a-10;
+	printf ("a = %d\n", a);
+	a = a/10;
+	printf ("a = %d\n", a);
+	return 0;
+}
+*/
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
 	int a, b;
 	
-	a = 23;
-	b = 24;
-	printf ("%d\n", a>b);
-	printf ("%d\n", a<b);
+	a = 20;
+	b = 5;
 	
+	a += b;
+	printf ("a = %d\n", a);
+	a *= b;
+	printf ("a = %d\n", a);
+	a -= b;
+	printf ("a = %d\n", a);
+	a /= b;
+	printf ("a = %d\n", a);
+	a %= b;
+	printf ("a = %d\n", a);
+	return 0; 
+}
+*/
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a = 10;
+	
+	a++;
+	a++;
+	printf ("a = %d\n", a);
+	++a;
+	++a;
+	printf ("a = %d\n", a);
+	
+	a = 10;
+	a--;
+	printf ("a = %d\n", a);
+	--a;
+	printf ("a = %d\n", a);
+	int b=10;
+	b = a++;
+	printf ("%d\n", b);
+	b = ++a;
+	printf ("%d\n", b);
+	return 0;
+}
+*/
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int num1 = 10;
+	int num2 = 20;
+	
+	int num3 = ++num1 + num2++;
+	
+	printf ("num1 = %d\n", num1);
+	printf ("num2 = %d\n", num2);
+	printf ("num3 = %d\n", num3);
+	return 0;
+}
+*/
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a = 20;
+	a += 4;
+	printf ("a = %d\n", a);
+	a /= 2;
+	printf ("a = %d\n", a);
+	--a;
+	printf ("a = %d\n", a);
+	a *= a;
+	printf ("a = %d\n", a);
+	return 0;	
+}
+*/
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a = 10;
+	int b = 2;
+	a -= b;
+	printf ("a = %d\n", a);
+	a *= b;
+	printf ("a = %d\n", a);
+	a--;
+	printf ("a = %d\n", a);
+	a /= a;
+	printf ("a = %d\n", a);
+	a += b;
+	printf ("a = %d\n", a);
+	a++;
+	printf ("a = %d\n", a);
+	a -= b;
+	printf ("a = %d\n", a);
 	return 0;
 }
 */
@@ -19,38 +135,19 @@ int main() {
 #include <stdlib.h>
 int main()
 {
-	printf ("%d\n", 10 > 5 && 8 < 3);
-	printf ("%d\n", 10 > 5 || 8 < 3);
-	printf ("%d\n", !(10>8));
-	return 0;
-}
-*/
-/*
-#include <stdio.h>
-#include <stdlib.h>
-int main ()
-{
-	int res;
+	int a, b;
 	
-	res = 0 && 0;
-	printf ("%d\n", res);
-	res = 0 && 1;
-	printf ("%d\n", res);
-	res = 1 && 0;
-	printf ("%d\n", res);
-	res = 1 && 1;
-	printf ("%d\n", res);
+	a = 10;
+	b = a++;
+	printf ("a = %d b = %d\n", a, b);
+	b = ++a;
+	printf ("a = %d b = %d\n", a, b);
 	
-	res = 0 || 0;
-	printf ("%d\n", res);
-	res = 0 || 1;
-	printf ("%d\n", res);
-	res = 1 || 0;
-	printf ("%d\n", res);
-	res = 1 || 1;
-	printf ("%d\n", res);
-	scanf ("%d", &res);
-	printf ("%d", res%2 == 0);
+	a = 20;
+	b = a--;
+	printf ("a = %d b = %d\n", a, b);
+	b = --a;
+	printf ("a = %d b = %d\n", a, b);
 	return 0;
 }
 */
@@ -59,43 +156,48 @@ int main ()
 #include <stdlib.h>
 int main()
 {
-	int a;
-	scanf ("%d", &a);
-	printf ("%d\n", a/7 == 5);
-	printf ("%d\n", a%3 == 0);
-	printf ("%d\n", !(a%5 == 0));
-	printf ("%d\n", a%3 == 0 && a%2 == 0);
-	printf ("%d\n", a%4 == 0 || a%7 == 0);
+	int num = 3;
+	
+	printf ("%d\n", num -= 1);
+	printf ("%d\n", --num);
+	printf ("%d\n", num--);
+	printf ("%d\n", num);
 	return 0;
 }
 */
 /*
 #include <stdio.h>
 #include <stdlib.h>
-int main ()
-{	
-	int a;
-	int b;
-	int c;
-	printf ("\"천재는 1%%의 영감과 99%%의 노력으로 이루어진다.\"\n\t-토머스 에디슨\n");
-	printf ("정수 1 : ");
-	scanf ("%d", &a);
-	printf ("정수 2 : ");
-	scanf ("%d", &b);
-	printf ("\n%d+%d = %d\n%d-%d = %d\n%d*%d = %d\n%d/%d = %d\n", a, b, a+b, a, b, a-b, a, b, a*b, a, b, a/b);
-	printf ("세자리 정수를 입력하세요.\n");
-	scanf ("%d", &c);
-	printf ("%d+%d+%d = %d\n", c/100, (c/10)-(c/100)*10, c%10, c/100+((c/10)-(c/100)*10)+c%10);
-	printf ("%d*%d*%d = %d", c/100, (c/10)-(c/100)*10, c%10, (c/100)*((c/10)-(c/100)*10)*(c%10));
+int main()
+{
+	int a = 12;
+	int b = 3;
+	
+	a -= b++; 
+	printf ("a = %d b = %d\n", a, b);
+	a /= --b;
+	printf ("a = %d b = %d\n", a, b);
+	a += b++;
+	printf ("a = %d b = %d\n", a, b);
+	a -= ++b;
+	printf ("a = %d b = %d\n", a, b);
+	a += --b;
+	printf ("a = %d b = %d", a, b);
 	return 0;
+	
 }
 */
 #include <stdio.h>
 #include <stdlib.h>
 int main()
 {
-	printf ("%d\n", 5000*2 > 1000*15);
-	printf ("%d\n", 1000/1 + 100 == 1000/3);
-	printf ("%d\n", 5000 >= 500*5 + 1000*2);
-	printf ("%d", 10000 > 2000*4 || 5000>2000*3);
+	int s;
+	int m;
+	int h;
+	scanf ("%d", &s);
+	h = s/3600;
+	m = (s-3600*h)/60;
+	s %= 60;
+	printf ("%d시 %d분 %d\n", h, m, s);
+	return 0;
 }
