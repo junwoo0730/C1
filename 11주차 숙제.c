eeccc37ec76842b3497d84e9e617c526f0ad12be
junwoo0730 <136076692+junwoo0730@users.noreply.github.com>
2023-08-24 23:00:04 +0900

    Add files via upload

---
+++ 11주차 숙제.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() 
+{	
+	int i;
+	int a;
+	int b;
+	int c;
+	for (i = 0; i < 5; i++)
+	{
+		for (a = 1; a < 4; a++)
+		{
+			printf (" ");
+		}
+		for (b = 1; b < b+1; b++)
+		{
+			printf ("%d", b);
+		}
+		printf ("\n");
+	}
+	return 0;
+}
